41db61143037b4632a70a198b7b8d96b735ad691
Sondre Kolberg <sondrkol@stud.ntnu.no>
2023-02-09 15:40:59 +0100

    Basic layout for homepage
    Co-author @norakm
    
    Issue #2

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,97 @@
 import './App.css';
 import handleSubmit from './handles/handlesubmit';
 import { useRef } from 'react';
- 
+
 function App() {
-  const dataRef = useRef()
- 
-  const submithandler = (e) => {
-    e.preventDefault()
-    handleSubmit(dataRef.current.value)
-    dataRef.current.value = ""
-  }
- 
-  return (
-    <div className="App">
-      <form onSubmit={submithandler}>
-        <input type= "text" ref={dataRef} />
-        <button type = "submit">Save</button>
-      </form>
-    </div>
-  );
+    const dataRef = useRef()
+    const submithandler = (e) => {
+        e.preventDefault()
+        handleSubmit(dataRef.current.value)
+        dataRef.current.value = ""
+    }
+
+    return (
+        <div>
+            <div class="GroupBar">
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+                <a class="GroupItem">PlaceHolder</a>
+            </div>
+            <div class="FeedContainer">
+                <div class="Feed">
+                    <p>heiFirst</p>
+                    <p>hei</p>
+                    <p>hei</p>
+                    <p>hei</p>
+                    <p>hei</p>
+                    <p>hei</p>
+                    <p>hei</p>
+                    <p>hei</p>
+                    <p>hei</p>
+                    <p>hei</p>
+                    <p>hei</p>
+                    <p>hei</p>
+                    <p>hei</p>
+                    <p>hei</p>
+                    <p>hei</p>
+                    <p>hei</p>
+                    <p>hei</p>
+                    <p>hei</p>
+                    <p>hei</p>
+                    <p>hei</p>
+                    <p>hei</p>
+                    <p>hei</p>
+                    <p>hei</p><p>hei</p>
+                    <p>hei</p>
+                    <p>hei</p>
+                    <p>hei</p><p>hei</p><p>hei</p><p>hei</p><p>heiLast</p>
+                </div>
+            </div>
+            <div class="MenubarContainer">
+                <table class="Menubar">
+                    <tr>
+                        <th class="MenuItem"><button class="MenuItemButton"></button></th>
+                        <th class="MenuItem"><button class="MenuItemButton"></button></th>
+                        <th class="MenuItem"><button class="MenuItemButton"></button></th>
+                        <th class="MenuItem"><button class="MenuItemButton"></button></th>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    );
 }
- 
+
 export default App;
