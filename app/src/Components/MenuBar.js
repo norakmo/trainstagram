cd050b068d336177cd63185f321e2be5855f0e0a
Sondre Kolberg <sondrkol@stud.ntnu.no>
2023-02-15 12:01:42 +0100

    Updated Menubar
    Issue #2

--- app/src/Components/MenuBar.js
+++ app/src/Components/MenuBar.js
@@ -1,23 +1,45 @@
 import React from "react";
 import './MenuBar.css';
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
 import HomeIcon from '@mui/icons-material/Home';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 
-export default class MenuBar extends React.Component{
+export default class MenuBar extends React.Component {
 
-    render(){
+    
+
+    render() {
+
+        let value = "Feed";
+
         return (
             <div class="MenubarContainer">
-                    <table class="Menubar">
+                {/* <table class="Menubar">
                         <tr>
                             <th class="MenuItem"><a class="MenuItemButton" href="./Feed"><HomeIcon sx="font-size: 300%;"/></a></th>
                             <th class="MenuItem"><a class="MenuItemButton" href="./Feed"><HomeIcon sx="font-size: 300%;"/></a></th>
                             <th class="MenuItem"><a class="MenuItemButton" href="./Feed"><HomeIcon sx="font-size: 300%;"/></a></th>
                             <th class="MenuItem"><a class="MenuItemButton" href="./Profile"><AccountBoxIcon sx="font-size: 300%;"/></a></th>
                         </tr>
-                    </table>
+                    </table> */}
+                <BottomNavigation
+                    showLabels
+                    value={value}
+                    onChange={(event, newValue) => {
+                        const navBarMap = new Map();
+                        navBarMap.set(0, "/Feed");
+                        navBarMap.set(1, "/Feed");
+                        navBarMap.set(2, "/Profile");
+                        value=newValue;
+                        window.location.href = navBarMap.get(newValue);
+                        console.log(newValue);
+                    }}
+                >
+                    <BottomNavigationAction label="Feed" icon={<HomeIcon />} />
+                    <BottomNavigationAction label="Stuff" icon={<HomeIcon />} />
+                    <BottomNavigationAction label="Profie" icon={<AccountBoxIcon />} />
+                </BottomNavigation>
             </div>
         )
     }
--- app/src/Components/ProfilePage.js
+++ app/src/Components/ProfilePage.js
@@ -2,32 +2,48 @@ import React from "react";
 import './ProfilePage.css';
 import Avatar from '@mui/material/Avatar';
 import PropTypes from "prop-types";
-import Profilbilde from './Profilbilde.png'
+import Profilbilde from './Profilbilde.png';
+import SettingsIcon from '@mui/icons-material/Settings';
+import Button from "@mui/material/Button";
 
 
 
 export default class ProfilePage extends React.Component{
 
+    
+
+    state={
+        name: "Navn navnesen",
+        DateOfBirth: "01.01.01",
+        height: 180,
+        weight: 80,
+        gender: "Male"
+    }
+
     render(){
         return(
             <div>
-                <div class="TopBar">
-                    <button class="EditProfileButton">
-                        
-                    </button>
-                </div>
                 <div class="ProfilePage">
                     <div class="ProfilePicture">
-                        <img class="Picture" src={Profilbilde} alt="ProfilePicture"></img>
+                        <Avatar sx="width: 200px; height: 200px; margin: auto;" src={Profilbilde} alt="ProfilePicture"/>
                     </div>
                     <div class="ProfileInfoBox">
-                        <h1>Navn Navnesen</h1>
-                        <p>Fødselsdato: {this.props.date}</p>
-                        <p>Høyde: </p>
-                        <p>Vekt: </p>
-                        <p>Kjønn: </p>
-                        <button class="MyWorkoutsButton">Mine Økter</button>
+                        <h1>{this.state.name}</h1>
+                        <p>Fødselsdato: {this.state.DateOfBirth}</p>
+                        <p>Høyde: {this.state.height}</p>
+                        <p>Vekt: {this.state.weight}</p>
+                        <p>Kjønn: {this.state.gender}</p>
+                        <Button variant="contained" 
+                        onClick={() =>{
+                            //this.setState({name: "Bror"})
+                        }}
+                        >Mine Økter</Button>
                     </div>
+                </div>
+                <div class="TopBar">
+                    <Button sx="right: 5px;">
+                        <SettingsIcon/>
+                    </Button>
                 </div>
             </div>
         )
