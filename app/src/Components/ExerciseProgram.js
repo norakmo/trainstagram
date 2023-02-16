d3bcc52ee67ed9cd47f07934b144ffa17648de7d
Tarjei Reite <tarjeireite@dhcp-10-24-7-216.wlan.ntnu.no>
2023-02-16 17:42:02 +0100

    Oppdatert med ulike Cards for programmer

--- app/src/Components/ExerciseProgram.js
+++ app/src/Components/ExerciseProgram.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Avatar from '@mui/material/Avatar';
-import { Button, Card, CardContent, Grid, Typography, AppBar, IconButton, Toolbar } from '@mui/material';
+import { Box, Button, Card, CardContent, Typography, AppBar, IconButton, Toolbar, CardActionArea } from '@mui/material';
 import {ArrowBack} from '@mui/icons-material';
 import PropTypes from "prop-types";
 import Profilbilde from './Profilbilde.png'
@@ -22,11 +22,68 @@ export default class ExerciseProgram extends React.Component{
                             </Typography>
                         </Toolbar>
                     </AppBar>
+
+                    <div>
+                        <Box style={{ backgroundColor: '#FBBF77' }} border={5} borderColor="white" borderRadius={2}>
+                            <Card style={{border: "0px solid black"}}>
+                                <CardActionArea>
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            Helvetesuke
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary" component="p">
+                                            7 dager, 7 økter
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                                <Button size="small" color="primary">
+                                    Se mer
+                                </Button>
+                            </Card>
+                        </Box>
+
+                        <Box style={{ backgroundColor: '#FBBF77' }} border={5} borderColor="white" borderRadius={2}>
+                            <Card style={{border: "0px solid black"}}>
+                                <CardActionArea>
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            Rolig uke
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary" component="p">
+                                            7 dager, 4 økter
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                                <Button size="small" color="primary">
+                                    Se mer
+                                </Button>
+                            </Card>
+                        </Box>
+
+                        <Box style={{ backgroundColor: '#FBBF77' }} border={5} borderColor="white" borderRadius={2}>
+                            <Card style={{border: "0px solid black"}}>
+                                <CardActionArea>
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            Muscle Month
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary" component="p">
+                                            30 dager, 20 økter
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+                                <Button size="small" color="primary">
+                                    Se mer
+                                </Button>
+                            </Card>
+                        </Box>
+                    </div>
+
                     <div style={{ display: 'flex', justifyContent: 'center', marginTop: 10 }}>
-                    <Button variant="contained" color="primary">
-                    Legg Til Nytt
-                    </Button>
-                </div>
+                        <Button variant="contained" color="primary">
+                            Legg Til Nytt
+                        </Button>
+                    </div>
                 </div>
             </div>
         )
