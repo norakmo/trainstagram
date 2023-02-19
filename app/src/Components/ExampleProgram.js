affe1fa21f5ce1709d93c452628f78fa4401efca
Tarjei Reite <tarjeireite@Tarjeis-MacBook-Pro.local>
2023-02-19 14:16:46 +0100

    Gjør et program skalerbart, slik at inputten nå er en array, hvor hvert element i arrayen tilsvarer en av dagene i programmet. Det kan være et vilkårlig antall økter på en dag

--- app/src/Components/ExampleProgram.js
+++ app/src/Components/ExampleProgram.js
@@ -5,13 +5,51 @@ import {ArrowBack} from '@mui/icons-material';
 
 const program = [
     [{type: "Cardio"}], 
+    [{type: "Styrke"}, {type: "Cardio"}], 
     [], 
-    [], 
-    [], 
-    [], 
-    [], 
+    [{type: "Styrke"}], 
+    [{type: "Styrke"}], 
+    [{type: "Cardio"}, {type: "Styrke"}], 
     [],
 ];
+
+const programliste = [];
+
+for (let i = 0; i < program.length; i++) {
+    
+    const sessions = [];
+
+    for (let j = 0; j < program[i].length; j++) {
+        const session = program[i][j]
+        const sessionColor = session.type === 'Cardio' ? '#99BADD' : '#FF7276';
+
+        sessions.push(
+            <Box border={10} borderColor="#FFFFFF" borderRadius={0}>
+                <Card>
+                    <CardActionArea style={{ backgroundColor: sessionColor }}>
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="h2">
+                                {session.type}
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+            </Box>
+        );
+    };
+
+    programliste.push(
+        <div>
+            <Box mb={1}>
+                <Typography variant="h6" ml={2}>Dag {i+1}</Typography>
+            </Box>
+            <Box border={1} borderColor="grey.400"></Box>
+            {sessions}
+        </div>
+    );
+};
+
+
 
 
 export default class ExampleProgram extends React.Component{
@@ -30,102 +68,7 @@ export default class ExampleProgram extends React.Component{
                         </Toolbar>
                     </AppBar>
 
-                    <div>
-                            <Box mb={1}>
-                                <Typography variant="h6" ml={2}>Dag 1</Typography>
-                            </Box>
-                            <Box border={1} borderColor="grey.400"></Box>
-
-                        <Box border={10} borderColor="#FFFFFF" borderRadius={0}>
-                            <Card>
-                                <CardActionArea style={{ backgroundColor: '#99BADD' }}>
-                                    <CardContent>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                            Cardio
-                                        </Typography>
-                                        <Typography variant="body2" color="textSecondary" component="p">
-                                            Intervall, 6x6 løping
-                                        </Typography>
-                                    </CardContent>
-                                </CardActionArea>
-                            </Card>
-                        </Box>
-
-                            <Box mb={1}>
-                                <Typography variant="h6" ml={2}>Dag 2</Typography>
-                            </Box>
-                            <Box border={1} borderColor="grey.400"></Box>
-                        
-                        <Box border={10} borderColor="#FFFFFF" borderRadius={0}>
-                            <Card>
-                                <CardActionArea style={{ backgroundColor: '#FF7276' }}>
-                                    <CardContent>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                            Styrke
-                                        </Typography>
-                                        <Typography variant="body2" color="textSecondary" component="p">
-                                            4 øvelser
-                                        </Typography>
-                                    </CardContent>
-                                </CardActionArea>
-                            </Card>
-                        </Box>
-
-                            <Box mb={1}>
-                                <Typography variant="h6" ml={2}>Dag 3</Typography>
-                            </Box>
-                            <Box border={1} borderColor="grey.400"></Box>
-                        
-                        <Box border={10} borderColor="#FFFFFF" borderRadius={0}>
-                            <Card>
-                                <CardActionArea style={{ backgroundColor: '#FF7276' }}>
-                                    <CardContent>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                            Styrke
-                                        </Typography>
-                                        <Typography variant="body2" color="textSecondary" component="p">
-                                            4 øvelser
-                                        </Typography>
-                                    </CardContent>
-                                </CardActionArea>
-                            </Card>
-                        </Box>
-
-                            <Box mb={1}>
-                                <Typography variant="h6" ml={2}>Dag 4</Typography>
-                            </Box>
-                            <Box border={1} borderColor="grey.400"></Box>
-
-                        <Box border={10} borderColor="#FFFFFF" borderRadius={0}>
-                            <Card>
-                                <CardActionArea style={{ backgroundColor: '#99BADD' }}>
-                                    <CardContent>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                            Cardio
-                                        </Typography>
-                                        <Typography variant="body2" color="textSecondary" component="p">
-                                            Langtur, 10 km, 60 min
-                                        </Typography>
-                                    </CardContent>
-                                </CardActionArea>
-                            </Card>
-                        </Box>
-
-                            <Box mb={1}>
-                                <Typography variant="h6" ml={2}>Dag 5</Typography>
-                            </Box>
-                            <Box border={1} borderColor="grey.400"></Box>
-
-                            <Box mb={1}>
-                                <Typography variant="h6" ml={2}>Dag 6</Typography>
-                            </Box>
-                            <Box border={1} borderColor="grey.400"></Box>
-
-                            <Box mb={1}>
-                                <Typography variant="h6" ml={2}>Dag 7</Typography>
-                            </Box>
-                            <Box border={1} borderColor="grey.400"></Box>
-                    </div>
+                    {programliste}
 
                     <div style={{ display: 'flex', justifyContent: 'center', marginTop: 10 }}>
                         <Button variant="contained" color="primary">
