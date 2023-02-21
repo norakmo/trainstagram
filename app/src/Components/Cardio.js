6e4edd2a3f40c782a75b9403b18d9fadd7a59c7c
Sondre Grøntvedt <sondregrontvedt@dhcp-10-22-11-133.wlan.ntnu.no>
2023-02-21 15:03:46 +0100

    issue #10: opprette side/frontend for opprettelse av treningsøkter

--- app/src/Components/Cardio.js
+++ app/src/Components/Cardio.js
@@ -2,6 +2,14 @@ import React from "react";
 import Collapsible from 'react-collapsible';
 import {Card} from '@mui/material';
 import "./Cardio.css";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
 
 
 export default class Cardio extends React.Component {
@@ -11,6 +19,7 @@ export default class Cardio extends React.Component {
         this.type = type;
         this.distance = distance;
         this.time = time;
+        this.exercises = [];
     }
 
     cardioExercises = [];
@@ -36,31 +45,58 @@ export default class Cardio extends React.Component {
         this.type = type;
     }
 
+
+
     render () {
         return (
-            <ul className="list">
-                <li>
-                    <Card>
-                        <Collapsible trigger="Intervall" id="Intervall">
-                            <input type="text"  placeholder="distance" onChange={(event) => {this.setDistance(parseInt(event.target.value))}}></input>
-                            <input type="text" placeholder="time" onChange={(event) => {this.setTime(parseInt(event.target.value))}}></input>
-                            <button onClick={() => this.cardioExercises.push(new Cardio(this.type, this.distance, this.time))}>add</button>
-                        </Collapsible>
-                    </Card>
-                </li>
-                <li>
-                    <Card>
-                        <Collapsible trigger="Langkjøring" id="Langkjøring">
-                            <input type="text" id="distance" name="distance" placeholder="distance" onChange={(event) => {this.setDistance(parseInt(event.target.value))}}></input>
-                            <input type="text" placeholder="time" onChange={(event) => {this.setTime(parseInt(event.target.value))}}></input>
-                            <button onClick={() => this.cardioExercises.push(new Cardio(this.type, this.distance, this.time))}>add</button>
-                        </Collapsible>
-                    </Card>
-                </li>
-                <li>
-                    <button onClick={() => console.log(this.cardioExercises)} >ShoppingCart</button>
-                </li>
-            </ul>
+            <div className="cardio_main">
+                <div className="cardio_wrapper">
+                    <h1 className="cardio_title">CARDIO</h1>
+                        <ul className="list">
+                            <li>
+                                <div className="cardio_container">
+                                <Card className="cardio_card">
+                                    <Collapsible trigger="Intervall" id="Intervall">
+                                        <input type="text"  placeholder="distance" onChange={(event) => {this.setDistance(parseInt(event.target.value))}}></input>
+                                        <input type="text" placeholder="time" onChange={(event) => {this.setTime(parseInt(event.target.value))}}></input>
+                                        <button onClick={() => this.cardioExercises.push(new Cardio(this.type, this.distance, this.time))}>add</button>
+                                    </Collapsible>
+                                </Card>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="cardio_container">
+                                <Card className="cardio_card">
+                                    <Collapsible trigger="Langkjøring" id="Langkjøring">
+                                        <input type="text" id="distance" name="distance" placeholder="distance" onChange={(event) => {this.setDistance(parseInt(event.target.value))}}></input>
+                                        <input type="text" placeholder="time" onChange={(event) => {this.setTime(parseInt(event.target.value))}}></input>
+                                        <button onClick={() => this.cardioExercises.push(new Cardio(this.type, this.distance, this.time))}>add</button>
+                                    </Collapsible>
+                                </Card>
+                                </div>
+                            </li>
+                        </ul>
+                        <TableContainer>
+                            <Table stickyHeader>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Type</TableCell>
+                                    <TableCell>Distance</TableCell>
+                                    <TableCell>Tid</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                <TableRow>
+                                    <TableCell>Langkjøring</TableCell>
+                                    <TableCell>10km</TableCell>
+                                    <TableCell>40min</TableCell>
+                                </TableRow>
+                            </TableBody>
+                            </Table>
+                        </TableContainer>
+                        <button className="cardio_shoppingCart" onClick={() => console.log(this.cardioExercises)} >ShoppingCart</button>
+                        </div>
+            </div>
         );
     }
 }
