30688108b2c0905578ab71511ef81c6a61140980
Sondre Grøntvedt <sondregrontvedt@dhcp-10-22-8-97.wlan.ntnu.no>
2023-02-22 14:15:28 +0100

    Issue: #10. Styling feed til treningsprogram good

--- app/src/Components/Cardio.js
+++ app/src/Components/Cardio.js
@@ -9,6 +9,8 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
 
 
 
@@ -19,28 +21,41 @@ export default class Cardio extends React.Component {
         this.type = type;
         this.distance = distance;
         this.time = time;
-        this.exercises = [];
+        this.rows = [];
+    }
+
+    handleAddRow = () => {
+        if (!this.distance || !this.time) {
+            return;
+        }
+        const newRow = { id: this.rows.length + 1, distance: this.distance + 'km', time: this.time + 'min' };
+        this.rows.push(newRow);
+        this.distance = '';
+        this.time = '';
+        this.forceUpdate();
+    };
+
+    handleDeleteRow = (id) => {
+        this.rows = this.rows.filter(row => row.id !== id);
+        this.forceUpdate();
     }
 
     cardioExercises = [];
-    // acceptedTypes = ["Intervall", "Langkjøring"];
-
-    // addCardioExercise(type, distance, time) {
-    //     let cardio = new Cardio(type, distance, time);
-    //     this.cardioExercises.push(cardio);
-    // }
-
-    // cardioEx() {
-    //     return this.cardioExercises;
-    // }
 
     setDistance(distance) {
+        if (!distance){
+            return;
+        }
         this.distance = distance;
     };
 
     setTime(time) {
+        if(!time){
+            return;
+        }
         this.time = time;
     };
+
     setType(type) {
         this.type = type;
     }
@@ -59,7 +74,7 @@ export default class Cardio extends React.Component {
                                     <Collapsible trigger="Intervall" id="Intervall">
                                         <input type="text"  placeholder="distance" onChange={(event) => {this.setDistance(parseInt(event.target.value))}}></input>
                                         <input type="text" placeholder="time" onChange={(event) => {this.setTime(parseInt(event.target.value))}}></input>
-                                        <button onClick={() => this.cardioExercises.push(new Cardio(this.type, this.distance, this.time))}>add</button>
+                                        <button onClick={() => {this.cardioExercises.push(new Cardio(this.type, this.distance, this.time)); this.handleAddRow();}}>add</button>
                                     </Collapsible>
                                 </Card>
                                 </div>
@@ -70,32 +85,39 @@ export default class Cardio extends React.Component {
                                     <Collapsible trigger="Langkjøring" id="Langkjøring">
                                         <input type="text" id="distance" name="distance" placeholder="distance" onChange={(event) => {this.setDistance(parseInt(event.target.value))}}></input>
                                         <input type="text" placeholder="time" onChange={(event) => {this.setTime(parseInt(event.target.value))}}></input>
-                                        <button onClick={() => this.cardioExercises.push(new Cardio(this.type, this.distance, this.time))}>add</button>
+                                        <button onClick={() => {this.cardioExercises.push(new Cardio(this.type, this.distance, this.time)); this.handleAddRow();}}>add</button>
                                     </Collapsible>
                                 </Card>
                                 </div>
                             </li>
                         </ul>
-                        <TableContainer>
-                            <Table stickyHeader>
+                        <TableContainer component={Paper}>
+                            <Table aria-label="simple table">
                             <TableHead>
                                 <TableRow>
                                     <TableCell>Type</TableCell>
-                                    <TableCell>Distance</TableCell>
-                                    <TableCell>Tid</TableCell>
+                                    <TableCell align="right">Distance</TableCell>
+                                    <TableCell align="right">Tid</TableCell>
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                <TableRow>
-                                    <TableCell>Langkjøring</TableCell>
-                                    <TableCell>10km</TableCell>
-                                    <TableCell>40min</TableCell>
+                            {this.rows.map((row) => (
+                                <TableRow key={row.id}>
+                                    <TableCell component="th" scope="row">{row.type}</TableCell>
+                                    <TableCell align="right">{row.distance}</TableCell>
+                                    <TableCell align="right">{row.time}</TableCell>
+                                    <TableCell align="center">
+                                    <IconButton onClick={() => this.handleDeleteRow(row.id)}>
+                                    <DeleteIcon />
+                                    </IconButton>
+                                </TableCell>
                                 </TableRow>
+                                ))}
                             </TableBody>
                             </Table>
                         </TableContainer>
                         <button className="cardio_shoppingCart" onClick={() => console.log(this.cardioExercises)} >ShoppingCart</button>
-                        </div>
+                    </div>
             </div>
         );
     }
