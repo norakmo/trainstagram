d7517f7aecceca48a0dca24523abb6919ba0d307
Sondre Kolberg <sondrkol@stud.ntnu.no>
2023-02-23 09:57:18 +0100

    Get Training Data

--- app/src/Components/Feed.js
+++ app/src/Components/Feed.js
@@ -3,16 +3,29 @@ import Collapsible from "react-collapsible";
 import './Feed.css';
 import FeedItem from './FeedItem';
 import { Card } from "@mui/material";
+import handleGetTrainingSessions from "../handles/handleGetTrainingSessions";
 
 export default class Feed extends React.Component{
+
+
+    componentDidMount(){
+        const TrainingSessions = handleGetTrainingSessions();
+        TrainingSessions.then((Sessions)=>{
+            this.setState({
+                sessions: Sessions
+            })
+        })
+    }
+
     render(){
         return(
             <div class="FeedContainer">
                 <div class="Feed">
-                        <FeedItem></FeedItem> 
-                        <FeedItem></FeedItem> 
-                        <FeedItem></FeedItem> 
-                        <FeedItem></FeedItem> 
+                        {
+                        this.state.sessions?.map((session)=>(
+                            <FeedItem props={{sessionData: session}}/>
+                        ))
+                        }
     
                 </div>
             </div>
