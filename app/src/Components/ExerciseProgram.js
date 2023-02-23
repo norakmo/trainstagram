16fd878506e1c07a5228afd1ab740bf19ad52483
Tarjei Reite <tarjeireite@dhcp-10-24-4-225.wlan.ntnu.no>
2023-02-23 07:56:21 +0100

    Jobb med å legge til programmer

--- app/src/Components/ExerciseProgram.js
+++ app/src/Components/ExerciseProgram.js
@@ -1,15 +1,89 @@
-class ExerciseProgram {
-    constructor(duration) {
-        this.duration = duration;
-        this.sessions = [];
-        this.sessionToDay = new Map();
-    }
 
-    addExerciseSession(session, day) {
-        if (day > this.duration) {
-            throw new Error("Den oppgitte dagen er utenfor programmets tidsrom.");
-        }
-        this.sessions.push(session);
-        this.sessionToDay.set(this.sessions.length, day);
-    }
-}
+
+
+
+const trainingSessions = [
+    {
+        name: "Styrke 1",
+        numberOfExercises: 3,
+        exercises: [
+            {
+                name: "Benk",
+                sets: 3,
+                reps: 8,
+                vekt: 50,
+            },
+            {
+                name: "Situps",
+                sets: 3,
+                reps: 30,
+                vekt: 50,
+            },
+            {
+                name: "Pushups",
+                sets: 3,
+                reps: 20,
+                rest: 60,
+            },
+        ],
+    },
+    {
+        name: "Styrke 2",
+        numberOfExercises: 2,
+        exercises: [
+            {
+                name: "Squats",
+                sets: 3,
+                reps: 10,
+                vekt: 50,
+            },
+            {
+                name: "Situps",
+                sets: 3,
+                reps: 30,
+                vekt: 50,
+            },
+        ],
+    },
+    {
+        name: "Styrke 3",
+        numberOfExercises: 1,
+        exercises: [
+            {
+                name: "Markløft",
+                sets: 3,
+                reps: 10,
+                vekt: 100,
+            },
+        ],
+    },
+];
+
+
+  const workoutPrograms = [
+    {
+        id: 1,
+        name: "Kom i gang",
+        duration: 3,
+        days: [
+            [trainingSessions[0]],
+            [],
+            [trainingSessions[0], trainingSessions[2]],
+        ],
+    },
+    {
+        id: 2,
+        name: "Sterk uke",
+        duration: 7,
+        days: [
+            [trainingSessions[0]],
+            [trainingSessions[1]],
+            [trainingSessions[2]],
+            [trainingSessions[1]],
+            [trainingSessions[0]],
+            [trainingSessions[2]],
+            [trainingSessions[1]],
+        ],
+    },
+];
+
