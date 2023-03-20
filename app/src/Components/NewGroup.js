376dd2d4dcdf880cfc17a03ee46ad82a87ea07e6
Sunil <sunils@stud.ntnu.no>
2023-03-20 08:58:02 +0100

    newgroup

--- app/src/Components/NewGroup.js
+++ app/src/Components/NewGroup.js
@@ -1,12 +1,21 @@
 import React from "react";
+import List from "@mui/material/List";
 import handleGetFriends from "../handles/handleGetFriends";
+import FriendCard from "./FriendCard";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
+import Card from "@mui/material/Card";
+import TextField from "@mui/material/TextField";
 import "./FriendList.css";
+import handleUpdateFriends from "../handles/handleUpdateFriends";
+
+
 
 export default class NewGroup extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      //email: props.props.email,
+      email: props.props.email,
       friends: "Empty",
     };
     this.getFriendList();
@@ -27,7 +36,45 @@ export default class NewGroup extends React.Component {
 
   render() {
     return (
-     <h1>hello</h1>   
-    );
+      <List sx="margin-top: 25px;">
+      {this.state.friends === "Empty" ? (
+        <p>Loading</p>
+      ) : (
+        this.state.friends.map((friend) => (
+          <FriendCard
+            props={{ data: friend, remove: this.handleRemoveFriend }}
+          />
+        ))
+      )}
+      {this.state.addFriendState ? (
+        <Card sx="margin: 5px; padding: 5px;">
+          <form onSubmit={this.handleAddFriendComplete}>
+            <TextField
+              sx="width: 80%;"
+              helperText="email of friend to add"
+              id="emailOfNewFriend"
+              onChange={(e) => {
+                this.setState({
+                  emailToAdd: e.target.value,
+                });
+              }}
+            />
+            <Fab color="primary" aria-label="add" type="submit">
+              <AddIcon />
+            </Fab>
+          </form>
+        </Card>
+      ) : (
+        <Fab
+          color="primary"
+          aria-label="add"
+          onClick={this.handleAddFriend}
+          sx="margin: 5px;"
+        >
+          <AddIcon />
+        </Fab>
+      )}
+    </List>
+  );
   }
 }
