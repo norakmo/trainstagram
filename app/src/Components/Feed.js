26a658cf422873bbf5bd5dfa6e0381ba5ef81d97
Ole Erik Nordtømme <oleerik.nord@gmail.com>
2023-03-23 11:45:00 +0100

    jnfainer

--- app/src/Components/Feed.js
+++ app/src/Components/Feed.js
@@ -1,79 +1,84 @@
 import React from "react";
 import Collapsible from "react-collapsible";
-import './Feed.css';
-import FeedItem from './FeedItem';
+import "./Feed.css";
+import FeedItem from "./FeedItem";
 import { Card } from "@mui/material";
 import handleGetTrainingSessions from "../handles/handleGetTrainingSessions";
 import { getCurrentUser } from "./Auth";
 import GroupBar from "./GroupBar";
-import handleGetSessionInGroup from "../handles/handleGetSessionInGroup"
+import handleGetSessionInGroup from "../handles/handleGetSessionInGroup";
 
-export default class Feed extends React.Component{
-    constructor(props){
-        super(props)
+export default class Feed extends React.Component {
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            sessions: "empty"
-        }
-    }
-    
-    componentDidMount(){
-        this.loadFriendSessions();
-    }
+    this.state = {
+      sessions: "empty",
+    };
+  }
 
-    loadFriendSessions(){
-        this.setState({
-            sessions: "empty",
-        })
-        getCurrentUser().then((user)=>{
-            const TrainingSessions = handleGetTrainingSessions(user.email);
-            TrainingSessions.then((Sessions)=>{
-                console.log(Sessions)
-                this.setState({
-                    sessions: Sessions,
-                },
-                () =>
-                {
-                    console.log(this.state.sessions);
-                })
-            });
+  componentDidMount() {
+    this.loadFriendSessions();
+  }
 
-        })
-    }
+  loadFriendSessions() {
+    this.setState({
+      sessions: "empty",
+    });
 
-    loadGroup(name, admin){
-        this.setState({
-            sessions: "empty"
-        },()=>{
-            handleGetSessionInGroup(name).then((sessions)=>{
-            
-                this.setState({
-                    sessions: sessions
-                },()=>{
-                    console.log(this.state.sessions);
-                })
-            })
-        })
-        
-    }
-    
+    getCurrentUser().then((user) => {
+      const TrainingSessions = handleGetTrainingSessions(user.email);
+      TrainingSessions.then((Sessions) => {
+        console.log(Sessions);
+        this.setState(
+          {
+            sessions: Sessions,
+          },
+          () => {
+            console.log(this.state.sessions);
+          }
+        );
+      });
+    });
+  }
 
-    render(){
-        return(
-            <div className="Bar"> < GroupBar props={{parent: this}}/> 
-                <div class="FeedContainer">
-                    <div class="Feed">
-                        {
-                        this.state.sessions === "empty" ?
-                        <p>Loading</p>
-                        :
-                        this.state.sessions.map((session)=>(
-                            <FeedItem props={{sessionData: session}}/>
-                        ))
-                        }
-                    </div>
-                </div>
-            </div>
-        )
-    }
+  loadGroup(name, admin) {
+    this.setState(
+      {
+        sessions: "empty",
+      },
+      () => {
+        handleGetSessionInGroup(name).then((sessions) => {
+          this.setState(
+            {
+              sessions: sessions,
+            },
+            () => {
+              console.log(this.state.sessions);
+            }
+          );
+        });
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div className="Bar">
+        {" "}
+        <GroupBar props={{ parent: this }} />
+        <div class="FeedContainer">
+          <div class="Feed">
+            {this.state.sessions === "empty" ? (
+              <p>Loading</p>
+            ) : (
+              this.state.sessions.map((session) => (
+                <FeedItem props={{ sessionData: session }} />
+              ))
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- app/src/Components/GroupBar.js
+++ app/src/Components/GroupBar.js
@@ -1,78 +1,69 @@
 import React from "react";
-import './GroupBar.css';
+import "./GroupBar.css";
 import handleGroupBar from "../handles/handleGroupBar";
 import Collapsible from "react-collapsible";
 import { getCurrentUser } from "./Auth";
 import BottomNavigation from "@mui/material/BottomNavigation";
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import FriendsList from "./FriendsList";
 import GroupBarElement from "./GroupBarElement";
-import GroupIcon from '@mui/icons-material/Group';
+import GroupIcon from "@mui/icons-material/Group";
 import { touchRippleClasses } from "@mui/material";
+import FeedItem from "./FeedItem";
 
+export default class GroupBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      groups: null,
+      showFriends: false,
+      parent: props.props.parent,
+      selected: "friends",
+    };
+  }
 
-
-export default class GroupBar extends React.Component{
-
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            groups: null,
-            showFriends: false,
-            parent: props.props.parent,
-            selected: "friends"
-        };
+  getGroup(name, admin) {
+    if (this.state.selected !== "friends") {
+      document.getElementById(this.state.selected).style.color = "black";
     }
+    console.log("getgroup");
+    if (this.state.selected === name) {
+      this.setState({
+        selected: "friends",
+      });
 
-
-
-    getGroup(name, admin){
-        if(this.state.selected !== "friends"){
-            document.getElementById(this.state.selected).style.color = "black";
-        }
-        console.log("getgroup")
-        if(this.state.selected === name){
-            this.setState({
-                selected: "friends"
-            })
-                
-            this.state.parent.loadFriendSessions();
-        }else{
-            this.setState({
-                selected: name
-            })
-            this.state.parent.loadGroup(name, admin);
-            document.getElementById(name).style.color = "white";
-        }
+      this.state.parent.loadFriendSessions();
+    } else {
+      this.setState({
+        selected: name,
+      });
+      this.state.parent.loadGroup(name, admin);
+      document.getElementById(name).style.color = "white";
     }
+  }
 
-    handleNewGroup(){
-        window.location.href = "/newGroup";
-    }
+  handleNewGroup() {
+    window.location.href = "/newGroup";
+  }
 
-    componentDidMount(){
-        getCurrentUser().then((user)=>{
-            handleGroupBar(user.email).then((groups)=>{
-                this.setState({
-                    groups: groups,
-                })
-            })
-        })
-    }
+  componentDidMount() {
+    getCurrentUser().then((user) => {
+      handleGroupBar(user.email).then((groups) => {
+        this.setState({
+          groups: groups,
+        });
+      });
+    });
+  }
 
-    render() {
-        
-        let value = "Feed";
+  render() {
+    let value = "Feed";
 
-    return(
-            <div class="GroupBarContainer">
-                <div class="GroupBar">
-                    
-                    
-                    
-                    {/* <BottomNavigation class ="nav"
+    return (
+      <div class="GroupBarContainer">
+        <div class="GroupBar">
+          {/* <BottomNavigation class ="nav"
                         showLabels
                         value={value}
                         onChange={(event, newValue) => {
@@ -87,25 +78,28 @@ export default class GroupBar extends React.Component{
                         <AddCircleOutlineIcon class ="Icon"/>} onClick={this.handleShowFriends}/>
                     </BottomNavigation> */}
 
-                    <fab onClick={this.handleNewGroup} label="new group">
-                        <AddCircleOutlineIcon class ="Icon"/>
-                        
-                    </fab>
-                    {
-                        this.state.groups == null ?
-                        <div/>
-                        :
-                        this.state.groups.map((group)=>(
-                            <fab onClick={()=>{this.getGroup(group[0], group[1])}} id={group[0]}>
-                                <div className="groupIconContainer">
-                                    <GroupIcon class="Icon"></GroupIcon>
-                                    <div>{group[0]}</div>
-                                </div>
-                            </fab>
-                        ))
-                    }
+          <fab onClick={this.handleNewGroup} label="new group">
+            <AddCircleOutlineIcon class="Icon" />
+          </fab>
+          {this.state.groups == null ? (
+            <div />
+          ) : (
+            this.state.groups.map((group) => (
+              <fab
+                onClick={() => {
+                  this.getGroup(group[0], group[1]);
+                }}
+                id={group[0]}
+              >
+                <div className="groupIconContainer">
+                  <GroupIcon class="Icon"></GroupIcon>
+                  <div>{group[0]}</div>
                 </div>
-            </div>
-        );
-    }
+              </fab>
+            ))
+          )}
+        </div>
+      </div>
+    );
+  }
 }
